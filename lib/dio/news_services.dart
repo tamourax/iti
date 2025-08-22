72c9ba16851e891b6e6a7706da5b7b18d3ea2bbe
tamourax <at074402@gmail.com>
2025-08-22 07:55:39 +0300

    fetch titles of the articles

--- lib/dio/news_services.dart
+++ lib/dio/news_services.dart
@@ -1,13 +1,21 @@
 import 'package:dio/dio.dart';
 
 class NewsServices {
-final dio = Dio();
+  final dio = Dio();
 
-void getGereralNews() async {
-  final response = await dio.get(
-    'https://newsapi.org/v2/everything?q=general&apiKey=e4ea152c161c4c91a9faad351c15e97c',
-  );
-  print(response);
-}
-
+  void getGereralNews() async {
+    Response response = await dio.get(
+      'https://newsapi.org/v2/everything?q=general&apiKey=e4ea152c161c4c91a9faad351c15e97c',
+    );
+    Map<String, dynamic> dataFromJson = response.data; // full response
+    List<dynamic> articles = dataFromJson['articles']; // response of articles
+    
+    
+    for (var i = 0; i < articles.length; i++) {
+      print(articles[i]['title']);  // response of all title in articles
+      
+    }
+ 
+   
+  }
 }
