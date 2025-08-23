e9af1d560d22bf4c2de939559811cfdf8d089184
tamourax <at074402@gmail.com>
2025-08-23 09:06:00 +0300

    convert List of json to new model and fetch it

---
+++ lib/models/new_model.dart
@@ -0,0 +1,12 @@
+class NewsModel {
+  String title;
+  String description;
+
+  String image;
+
+  NewsModel({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,23 +1,27 @@
 import 'package:dio/dio.dart';
+import 'package:iti/models/new_model.dart';
 
 class NewsServices {
   final dio = Dio();
 
   void getGereralNews() async {
     Response response = await dio.get(
-      'https://newsapi.org/v2/everything?q=general&apiKey=e4ea152c161c4c91a9faad351c15e97c',      
-
-      
+      'https://newsapi.org/v2/everything?q=general&apiKey=e4ea152c161c4c91a9faad351c15e97c',
     );
     Map<String, dynamic> dataFromJson = response.data; // full response
     List<dynamic> articles = dataFromJson['articles']; // response of articles
-    
-    
-    for (var i = 0; i < articles.length; i++) {
-      print(articles[i]['title']);  // response of all title in articles
-      
+
+    List<NewsModel> newsList = []; // list of news ui obj (ele ahna 3izenha)
+
+    for (var article in articles) {
+      NewsModel newsModel = NewsModel(
+        title: article['title'],
+        description: article['description'],
+        image: article['urlToImage'],
+      );
+
+      newsList.add(newsModel);
     }
- 
-   
+    print(newsList);
   }
 }
