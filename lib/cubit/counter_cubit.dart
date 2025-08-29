5c4a42454f0dacfc024d9831c2a06a24d596c5a4
tamourax <at074402@gmail.com>
2025-08-29 09:54:55 +0300

    create the function of counter

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -5,4 +5,20 @@ part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
   CounterCubit() : super(CounterInitial());
+  int counter = 0;
+
+  void increment() {
+    counter++;  
+  emit(CounterIncrement());
+
+  }
+  void decrement() 
+  {
+    counter--;
+    emit(CounterDecrement());
+  }
+  void reset() {
+    counter = 0;
+    emit(CounterReset());
+  }
 }
